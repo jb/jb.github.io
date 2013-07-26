20d1361d97bab62d4431212ba50b1190bea5f168
Josh Brown <joshbrown15@gmail.com>
2013-07-26 21:03:06 +0100

    Add AJAX navigation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,7 @@
 TIME = 300  // Number of milliseconds an action takes
 
 
-$('.email').each(function() {
-	$(this).html( $(this).html().replace('@@@', '@').replace(/@@@/g, '.') );
-	$(this).attr('href', $(this).attr('href').replace('@@@', '@').replace(/@@@/g, '.') );
-});
-
-
-$('#slideswrap').css('overflow-x', 'hidden');
-$('#slider ul a').click(function() {
+$('body').on('click', '#slider ul a', function() {
 	if ( $('#slider .cover').is(':visible') ) {
 		$('#slider .cover').fadeOut(TIME);
 		var TIME = 0;
@@ -30,5 +23,27 @@ $(window).resize(function() {
 	clearTimeout(resizeTimer);
 	resizeTimer = setTimeout(adjust_slides, 50);
 });
-$('#slider .cover').css('display', '-webkit-box').css('display', 'box');
-setTimeout(adjust_slides, 50);
+
+
+function init() {
+	$('#slideswrap').css('overflow-x', 'hidden');
+	$('#slider .cover').css('display', '-webkit-box').css('display', 'box');
+	setTimeout(adjust_slides, 50);
+
+	$('.email').each(function() {
+		$(this).html( $(this).html().replace('@@@', '@').replace(/@@@/g, '.') );
+		$(this).attr('href', $(this).attr('href').replace('@@@', '@').replace(/@@@/g, '.') );
+	});
+}
+$(init);
+
+
+$('body').on('click', 'header a', function() {
+	var href = $(this).attr('href');
+	history.pushState(null, null, href);
+	$('#main').load(href + ' #main', function() {
+		$(init);
+		ga('send', 'pageview');
+	});
+	return false;
+});
